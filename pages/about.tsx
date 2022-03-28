12539824d8c4514dc8cb1a3818b9096148e9a2b2
dawn1004 <dawn.bugay@hoolisoftware.com>
2022-03-29 02:19:00 +0800

    fix production error

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,7 +8,7 @@ const About = () => {
         <Layout >
             <>
                 <h1 className='text-2xl font-medium text-primary mb-2'>About This App</h1>
-                <p>Hi I'm Dawn and I create this app for my front-end exam which is one of the recruitment requirements here in ISR.</p>
+                <p>Hi Im Dawn and I create this app for my front-end exam which is one of the recruitment requirements here in ISR.</p>
                 <p className='mb-6'>Hope to pass this exam and work with you soon. Have a great day! {`:)`}</p>
                 <span onClick={()=>{router.push("/")}} className='text-primary underline cursor-pointer'>Back to homepage</span>
             </>
