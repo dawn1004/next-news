cfb8337b2a7dc13b4f13abf070161a9554c2268d
dawn1004 <dawn.bugay@hoolisoftware.com>
2022-03-29 02:36:22 +0800

    fix loading position

--- components/loading/LoadingOverlay.tsx
+++ components/loading/LoadingOverlay.tsx
@@ -9,7 +9,7 @@ interface ILoadingOverlay{
 const LoadingOverlay = ({isShow = false}: ILoadingOverlay) => {
   return (
     isShow?
-    <div className='flex justify-center items-center bg-gray-900/50 absolute top-0 left-0 w-full h-full z-50'>
+    <div className='flex justify-center items-center bg-gray-900/50 fixed top-0 left-0 w-full h-full z-50'>
         <div className="w-60">
             <Image src={Spinner} />
         </div>
